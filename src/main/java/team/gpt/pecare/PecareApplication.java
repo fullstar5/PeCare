796679a8cc5ac2ac162983570ab1f837b78f1f55
xZhou <xinzhou0417@gmail.com>
2024-04-04 15:15:18 +1100

    add basic configuration for springboot framework

---
+++ src/main/java/team/gpt/pecare/PecareApplication.java
@@ -0,0 +1,12 @@
+package team.gpt.pecare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PecareApplication {
+    public static void main(String[] args){
+        SpringApplication.run(PecareApplication.class, args);
+    }
+
+}

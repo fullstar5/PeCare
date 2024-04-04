116bd8d537dce1a1773ddcb1577a77a0f1b9487c
xZhou <xinzhou0417@gmail.com>
2024-04-05 03:30:38 +1100

    Revert "Add Quasar"
    This reverts commit e60416a963863699ea6cbc750c18922662a923f5.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,4 @@ import App from './App.vue'
 import { Quasar } from 'quasar'
 import quasarUserOptions from './quasar-user-options'
 
-createApp(App).use(Quasar, quasarUserOptions).use(Quasar, quasarUserOptions).mount('#app')
+createApp(App).use(Quasar, quasarUserOptions).mount('#app')

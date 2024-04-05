6697ef4c5ba53f57466ef8edbfcb6c472ef9daa4
xZhou <xinzhou0417@gmail.com>
2024-04-05 21:45:51 +1100

    configure api

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -2,15 +2,24 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 
+
 // Vuetify
 import 'vuetify/styles'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
 
 const vuetify = createVuetify({
   components,
-  directives
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
 })
 
 
--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,59 +3,92 @@ import { createMemoryHistory, createRouter } from 'vue-router'
 const routes = [
   {
     path: "/",
-    alias: "/Home",
+    alias: "/home",
     name: "Home",
-    component: () => import("@/views/Home")
+    component: () => import("@/views/Home"),
+    meta: {
+      title: 'Home'
+    }
   },
   {
     path: "/about",
     name: "about",
-    component: () => import("@/views/About")
+    component: () => import("@/views/About"),
+    meta: {
+      title: 'About Us'
+    }
   },
   {
     path: "/credit",
     name: "credit",
-    component: () => import("@/views/Credit")
+    component: () => import("@/views/Credit"),
+    meta: {
+      title: 'Credits'
+    }
   },
   {
     path: "/order",
     name: "order",
-    component: () => import("@/views/Order")
+    component: () => import("@/views/Order"),
+    meta: {
+      title: 'Orders'
+    }
   },
   {
     path: "/orderCreate",
     name: "orderCreate",
-    component: () => import("@/views/orderCreate")
+    component: () => import("@/views/OrderCreate"),
+    meta: {
+      title: 'Create Order'
+    }
   },
   {
     path: "/orderFinish",
     name: "orderFinish",
-    component: () => import("@/views/orderFinish")
+    component: () => import("@/views/OrderFinish"),
+    meta: {
+      title: 'Order Finished!'
+    }
   },
   {
     path: "/userHome",
     name: "userhome",
-    component: () => import("@/views/user/UserHome")
+    component: () => import("@/views/user/UserHome"),
+    meta: {
+      title: 'My Profile'
+    }
   },
   {
     path: "/userInfo",
     name: "userinfo",
-    component: () => import("@/views/user/UserInfo")
+    component: () => import("@/views/user/UserInfo"),
+    meta: {
+      title: 'Change User Profile'
+    }
   },
   {
     path: "/login",
     name: "login",
-    component: () => import("@/views/login/Login")
+    component: () => import("@/views/login/Login"),
+    meta: {
+      title: 'Login'
+    }
   },
   {
     path: "/fogotPswd",
     name: "fogotPswd",
-    component: () => import("@/views/login/ForgotPswd")
+    component: () => import("@/views/login/ForgotPswd"),
+    meta: {
+      title: 'Reset Password'
+    }
   },
   {
     path: "/signUp",
     name: "signUp",
-    component: () => import("@/views/login/signUp")
+    component: () => import("@/views/login/SignUp"),
+    meta: {
+      title: 'Sign Up'
+    }
   }
 ]
 
@@ -63,5 +96,18 @@ const router = createRouter({
   history: createMemoryHistory(),
   routes
 })
+
+router.beforeEach((to, from, next) => {
+  const title = to.meta.title
+  if (title) {
+    document.title = title
+  }
+  next()
+})
+
+router.afterEach((to, from, next) => {
+
+})
+
 
 export default router
